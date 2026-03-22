8c86817532f8fe04a9bf96977da90490adb6dab5
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2026-03-22 18:37:40 +0100

    fix: Simplify input validation by removing unused regex check

--- src/ts/validateInput.ts
+++ src/ts/validateInput.ts
@@ -1,17 +1,11 @@
 export function validateInput(input: HTMLInputElement, error: HTMLSpanElement, ) {
     const value = Number(input.value);
-    const regex = /^\d+(?:[.,]\d{1,2})?$/;
 
     if (input.value.trim() === '' || value <= 0) {
         input.classList.add('error-input');
         error.classList.add('show');
         error.classList.remove('hide');
-        error.innerHTML = 'Can\'t be zero';
-    } else if (!regex.test(input.value)) {
-        input.classList.add('error-input');
-        error.classList.add('show');
-        error.classList.remove('hide');
-        error.innerHTML = 'Not a valid number';
+        error.innerHTML += 'Can\'t be zero ';
     } else {
         input.classList.remove('error-input');
         error.classList.add('hide');
